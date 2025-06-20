3751dde0be878a773ea2f64cc49629b7ea9809f1
Matt Parrott <matthew.parrott@louisvillleky.gov>
2025-06-20 11:26:49 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Evaluation`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
